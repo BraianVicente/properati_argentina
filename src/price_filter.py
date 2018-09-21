875f9dce95dbed1506692dfc043f05419f16a4a8
Braian Hernán Vicente <bvicente@baufest.com>
2018-09-21 09:14:26 -0300

    adding GNU header

--- src/price_filter.py
+++ src/price_filter.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+#  price_filter.py
+#  
+#  Copyright 2018 Braian Hernan Vicente <bvicente@fi.uba.ar>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
 import pandas as pd
 import numpy as np
     # Los datos fueron provistos por  http://www.properati.com.ar y
